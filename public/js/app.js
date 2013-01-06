7095421f031c3313b30b008c9210058b3a21c0c0
deoxxa <deoxxa@fknsrs.biz>
2013-01-06 09:37:33 +0900

    added batching to draw events to reduce network usage

--- public/js/app.js
+++ public/js/app.js
@@ -7,39 +7,78 @@ var tablet = document.getElementById("tablet"),
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+var current_line = [],
+    last_position = null;
+
+var on_mousedown = function on_mousedown(ev) {
+  if (tablet.penAPI.pressure !== 0) {
+    current_line.push([ev.x, ev.y, tablet.penAPI.pressure]);
+  }
+
+  canvas.addEventListener("mousemove", on_mousemove);
+  canvas.addEventListener("mouseup", on_mouseup);
+};
+
+var on_mouseup = function on_mouseup() {
+  if (current_line.length > 1) {
+    socket.emit("draw", {type: "line", points: current_line});
+  } else if (current_line.length === 1) {
+    socket.emit("draw", {type: "point", point: current_line[0]});
+  }
+
+  current_line = [];
+  last_position = null;
+
+  canvas.removeEventListener("mousemove", on_mousemove);
+  canvas.removeEventListener("mouseup", on_mouseup);
+};
+
 var last_position = null;
 var on_mousemove = function on_mousemove(ev) {
   if (last_position) {
-    var data = {
-      from_x: last_position.x,
-      from_y: last_position.y,
-      to_x: ev.x,
-      to_y: ev.y,
-      pressure: tablet.penAPI.pressure,
-    };
-
-    on_draw(data);
-    socket.emit("draw", data);
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "rgba(0,0,0," + tablet.penAPI.pressure + ")";
+    ctx.moveTo(last_position[0], last_position[1]);
+    ctx.lineTo(ev.x, ev.y);
+    ctx.stroke();
   }
 
-  if (tablet.penAPI.pressure === 0) {
-    last_position = null;
-  } else {
-    last_position = {
-      x: ev.x,
-      y: ev.y,
-    };
+  if (tablet.penAPI.pressure !== 0) {
+    current_line.push([ev.x, ev.y, tablet.penAPI.pressure]);
+    last_position = [ev.x, ev.y];
   }
 };
 
 var on_draw = function on_draw(data) {
+  console.log("Drawing object of type: " + data.type);
+
+  if (data.type === "line") {
+    draw_line(data);
+  } else if (data.type === "point") {
+    draw_point(data);
+  }
+};
+
+var draw_line = function draw_line(data) {
+  for (var i=0;i<data.points.length-1;++i) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "rgba(0,0,0," + data.points[i+1][2] + ")";
+    ctx.moveTo(data.points[i][0], data.points[i][1]);
+    ctx.lineTo(data.points[i+1][0], data.points[i+1][1]);
+    ctx.stroke();
+  }
+};
+    
+var draw_point = function draw_point(data) {
   ctx.beginPath();
   ctx.lineWidth = 1;
-  ctx.strokeStyle = "rgba(0,0,0," + data.pressure + ")";
-  ctx.moveTo(data.from_x, data.from_y);
-  ctx.lineTo(data.to_x, data.to_y);
+  ctx.strokeStyle = "rgba(0,0,0," + data[2] + ")";
+  ctx.moveTo(data[0], data[1]);
+  ctx.lineTo(data[0], data[1]);
   ctx.stroke();
 };
 
-socket.on("connect", canvas.addEventListener.bind(canvas, "mousemove", on_mousemove));
+socket.on("connect", canvas.addEventListener.bind(canvas, "mousedown", on_mousedown));
 socket.on("draw", on_draw);
